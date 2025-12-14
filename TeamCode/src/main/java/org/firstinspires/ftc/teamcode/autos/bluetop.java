e346ac743c79bb8c033ebe8ff5774940282209f1
Kristi-Kwok <kristikwok5@gmail.com>
2025-12-14 15:17:54 -0800

    Added blue top auto and updated mecanum code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autos/bluetop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autos/bluetop.java
@@ -1,21 +1,20 @@
-package org.firstinspires.ftc.teamcode;
+package org.firstinspires.ftc.teamcode.autos;
+
 import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.CRServo;
-import com.qualcomm.robotcore.hardware.ColorSensor;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
-import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.IMU;
 import com.qualcomm.robotcore.hardware.ImuOrientationOnRobot;
 
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 
-@TeleOp(name = "kristiMecanum")
-public class kristiMecanum extends LinearOpMode {
+@Autonomous
+public class bluetop extends LinearOpMode {
     int counter = 0;
-    double power = 1;
+    int counter_loop = 0;
     double flywheelVel = 0;
     double targetFlywheelVel = 1500;
 
@@ -32,14 +31,14 @@ public class kristiMecanum extends LinearOpMode {
         CRServo left_launch_servo = hardwareMap.get(CRServo.class, "leftServo");
         IMU imu = hardwareMap.get(IMU.class, "imu");
 
-        // You don't HAVE to do this, but it makes things clear
+        //setting direction of each motor
         frontleft.setDirection(DcMotor.Direction.REVERSE);
         frontright.setDirection(DcMotor.Direction.FORWARD);
         backleft.setDirection(DcMotor.Direction.REVERSE);
         backright.setDirection(DcMotor.Direction.FORWARD);
 
         ImuOrientationOnRobot orientation = new RevHubOrientationOnRobot(
-                RevHubOrientationOnRobot.LogoFacingDirection.BACKWARD,
+                RevHubOrientationOnRobot.LogoFacingDirection.FORWARD,
                 RevHubOrientationOnRobot.UsbFacingDirection.LEFT);
 
         IMU.Parameters parameters = new IMU.Parameters(orientation);
@@ -48,48 +47,16 @@ public class kristiMecanum extends LinearOpMode {
         telemetry.addData("Status", "IMU Calibrating...");
         telemetry.update();
 
+        boolean shooting = false;
+
+        double power = 0.7;
         imu.resetYaw();
         waitForStart();
 
-        while (opModeIsActive()) {
-            double heading = imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.DEGREES);
-            flywheelVel = flywheel.getVelocity();
+        while (opModeIsActive()){
+            shooting = true;
 
-
-            double leftfrontPower = gamepad1.left_stick_y - gamepad1.left_stick_x - gamepad1.right_stick_x;
-            double rightfrontPower = gamepad1.left_stick_y + gamepad1.left_stick_x + gamepad1.right_stick_x;
-            double leftbackPower = gamepad1.left_stick_y + gamepad1.left_stick_x - gamepad1.right_stick_x;
-            double rightbackPower = gamepad1.left_stick_y - gamepad1.left_stick_x + gamepad1.right_stick_x;
-            frontleft.setPower(leftfrontPower);
-            frontright.setPower(rightfrontPower);
-            backleft.setPower(leftbackPower);
-            backright.setPower(rightbackPower);
-
-
-            telemetry.addData("Heading (Z)", heading);
-
-            telemetry.addData("front left motor", leftfrontPower);
-            telemetry.addData("front right motor", rightfrontPower);
-            telemetry.addData("back left motor", leftbackPower);
-            telemetry.addData("back right motor", rightbackPower);
-            telemetry.update();
-
-            if (gamepad1.dpad_left == true){
-                frontleft.setPower(1);
-                frontright.setPower(-1);
-                backleft.setPower(-1);
-                backright.setPower(1);
-            }
-
-            if (gamepad1.dpad_right == true){
-                frontleft.setPower(-1);
-                frontright.setPower(1);
-                backleft.setPower(1);
-                backright.setPower(-1);
-            }
-
-            if(gamepad1.right_bumper){
-
+            if (shooting == true){
                 double motorSpeedTowardsTarget;
 
                 if(flywheelVel < targetFlywheelVel) {
@@ -102,11 +69,9 @@ public class kristiMecanum extends LinearOpMode {
                         motorSpeedTowardsTarget = 1;
                     }
                     flywheel.setPower(motorSpeedTowardsTarget);
-                }
-                else {
+                } else {
                     flywheel.setPower(0);
                 }
-
 
                 counter += 1;
 
@@ -129,6 +94,36 @@ public class kristiMecanum extends LinearOpMode {
                 right_launch_servo.setPower(0);
                 left_launch_servo.setPower(0);
             }
-        }
+            frontleft.setPower(0);
+            frontright.setPower(0);
+            backleft.setPower(0);
+            backright.setPower(0);
+            sleep(10000);
+            //backwards .5 seconds
+            frontleft.setPower(-.7);
+            frontright.setPower(-.7);
+            backleft.setPower(-.7);
+            backright.setPower(-.7);
+            sleep(500);
+            //straf right
+            frontleft.setPower(-.7);
+            frontright.setPower(-.7);
+            backleft.setPower(.7);
+            backright.setPower(.7);
+            sleep(600);
+            frontleft.setPower(-.7);
+            frontright.setPower(-.7);
+            backleft.setPower(.7);
+            backright.setPower(.7);
+            sleep(500);
+            frontleft.setPower(0);
+            frontright.setPower(0);
+            backleft.setPower(0);
+            backright.setPower(0);
+
+
+
+
+            }
         }
     }
