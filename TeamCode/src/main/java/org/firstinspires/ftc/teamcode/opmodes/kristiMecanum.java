70b2ffd07c5ffa6d7e38e0fae47f9983d488a756
Kristi-Kwok <kristikwok5@gmail.com>
2026-01-09 16:23:24 -0800

    added a space

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/kristiMecanum.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/kristiMecanum.java
@@ -76,7 +76,7 @@ public class kristiMecanum extends LinearOpMode {
             double heading = imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.DEGREES);
             flywheelVel = flywheel.getVelocity();
 
-            if (gamepad1.aWasPressed()){
+            if (gamepad1.aWasPressed()) {
                 imu.resetYaw();
             }
 
